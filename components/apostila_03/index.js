04adf47a09180ac7623288ac86ebfd694517d483
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2023-05-09 20:48:51 -0300

    aula 09-05-23

--- components/apostila_03/index.js
+++ components/apostila_03/index.js
@@ -1,11 +1,19 @@
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
 export default function apostila03() {
   return (
     <View style={styles.container}>
-      <Text>Exemplo apostila 3</Text>
+      <Text style={styles.paragraph}>Exemplo apostila 3</Text>
+
+      <TouchableOpacity 
+        style={styles.button} 
+        onPress={() => alert('oi')}
+      >
+        <Text style={styles.textButton} >Hello World!!!</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
