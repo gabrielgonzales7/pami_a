4b97aeb1608efbe94494e31ce53a3d701adbbb3f
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2023-05-30 20:51:46 -0300

    aula 30-05

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native'; 
+import Constants from 'expo-constants';
 
 import Apostila03 from './components/apostila_03'; 
-import Apostila03A from './components/apostila_03_avancado';
+import Apostila03A from './components/apostila_03_avancado'; 
+import Apostila04 from './components/apostila_04';
+
+let corApp = '#fff';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Apostila03A />
-      <StatusBar style="auto" />
+      <Apostila04 />
+      <StatusBar style={corApp === '#000' ? "light" : "dark"} />
     </View>
   );
 }
@@ -16,8 +20,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: corApp, 
+    padding: 8, 
+    paddingTop: Constants.statusBarHeight, 
     //alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center', 
   },
 });
---
+++ components/apostila_04/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'; 
+import { View, Text } from 'react-native'; 
+
+import styles from './styles';
+
+function Apostila04() {
+    return(
+        <View style={styles.container}>
+            <Text style={styles.texto}>Exemplo 4 </Text>
+        </View>
+    );
+}
+
+export default Apostila04;
